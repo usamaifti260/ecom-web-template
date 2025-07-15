8d012fc2c2a6aceec7f26084908acab04b21a7bc
usamaifti536 <usamaiftikhar536@gmail.com>
2025-07-15 06:58:23 +0500

    Seventh Commit

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+'use client';
+import { useState} from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
